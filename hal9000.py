8a1c6b85bd684228713a87fc8c353f051c81d3f7
Ivan Renedo <renedo@gmail.com>
2019-09-04 12:24:44 +0200

    complete separation of hal9000 and modules reponses. New returning message format with emojis

--- hal9000.py
+++ hal9000.py
@@ -1,19 +1,28 @@
 from slackeventsapi import SlackEventAdapter
 from slack import WebClient
-from yaml import safe_load
+from yaml import safe_load, YAMLError
 import importlib
 import sys
 
-
-with open("config.yml", 'r') as stream:
-    try:
-        cfg = safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+try:
+    with open("config.yml", 'r') as stream:
+        try:
+            cfg = safe_load(stream)
+        except YAMLError as err:
+            print("config.yml is not a proper yaml file\n{}".format(err))
+            sys.exit(2)
+except Exception as err:
+    print("Error openning the configuration file{}\n".format(err))
+    sys.exit(2)
 
 config = cfg['config']
 sys.path.append('./packages')
-pkg = {mod:importlib.import_module(mod) for mod in cfg['modules'].keys()}
+
+try:
+    pkg = {mod:importlib.import_module(mod) for mod in cfg['modules'].keys()}
+except Exception as err:
+    print("Error importing modules:\n{}".format(err))
+    sys.exit(2)
 
 # Our app's Slack Event Adapter for receiving actions via the Events API
 slack_events_adapter = SlackEventAdapter(config['slack_signing_secret'], "/slack/events")
@@ -35,26 +44,41 @@ def handle_message(event_data):
             user = message['user']
             channel = message["channel"]
             thread_ts = message.get('ts')
-            ret = pkg[command].command(client,message.get('text').split(" ")[1:])
-            if ret['block']:
-                client.chat_postMessage(
-                    channel=channel,
-                    block=ret['message'][0],
-                    thread_ts=thread_ts
-                )                
+            try:
+                ret = pkg[command].command(message.get('text').split(" ")[1:])
+            except Exception as err:
+                print("Problem calling the module {}\n{}".format(command,err))
             else:
-                client.chat_postMessage(
-                    channel=channel,
-                    text=ret['message'][0],
-                    thread_ts=thread_ts
-                ) 
+                if ret['error']:
+                    mes = client.chat_postMessage(
+                        channel=channel,
+                        text="Error executing command: *{}*".format(ret['message']),
+                    )
+                else:
+                    if ret['block']:
+                        mes = client.chat_postMessage(
+                            channel=channel,
+                            block=ret['message'][0],
+                            thread_ts=thread_ts
+                        )                
+                    else:
+                        mes = client.chat_postMessage(
+                            channel=channel,
+                            text=ret['message'][0],
+                            thread_ts=thread_ts
+                        ) 
+                if ret['reactions'] is not []:
+                    for reaction in ret['reactions']:
+                        client.reactions_add(
+                          channel=channel,
+                          name=reaction,
+                          timestamp=mes['message']['ts']
+                        )
         else:
-            thread_ts = message.get('ts')
             channel = message.get('channel')
             client.chat_postMessage(
                 channel=channel,
-                text="Sorry, I didn't understand you :thinking_face:",
-                thread_ts=thread_ts
+                text="Sorry, I didn't understand you :thinking_face:"
             )
 
 # Error events
@@ -65,4 +89,7 @@ def error_handler(err):
 
 # Once we have our event listeners configured, we can start the
 # Flask server with the default `/events` endpoint on port 3000
-slack_events_adapter.start(host='0.0.0.0', port=3000)
+try:
+    slack_events_adapter.start(host='0.0.0.0', port=3000)
+except Exception as err:
+    print("Error starting the API server:\n{}".format(err))
--- packages/weather/__init__.py
+++ packages/weather/__init__.py
@@ -9,28 +9,40 @@ with open("config.yml", 'r') as stream:
 
 config = cfg['modules']['weather']
 
+def help()
 
-def command(client, args):
-    weather = {}
+def command(args):
     bot_return = {}
-    api_address = "https://api.openweathermap.org/data/2.5/weather?q={}" \
-                  "&appid={}" \
-                  "&units=metric".format(args[0], config['owappid'])
-    json_data = requests.get(api_address).json()
-    weather['current'] = json_data['main']['temp']
-    weather['pressure'] = json_data['main']['pressure']
-    weather['humidity'] = json_data['main']['humidity']
-    weather['temp_max'] = json_data['main']['temp_max']
-    weather['temp_min'] = json_data['main']['temp_min']
-    bot_return['message'] = "Current temperature: *{}°C*\n" \
-                            "Pressure: *{}Pa*\n" \
-                            "Humidity: *{}%*\n" \
-                            "Maximum temperature today: *{}°C*\n" \
-                            "Minimum temperature today: *{}°C*".format(weather['current'],
-                                                                       weather['pressure'],
-                                                                       weather['humidity'],
-                                                                       weather['temp_max'],
-                                                                       weather['temp_min']),
+    if args == []:
+      bot_return['error'] = True
+      bot_return['message'] = "No parameters"
+      bot_return['reactions'] = ["angry","eggplant"]
+      bot_return['block'] = False
+      return bot_return 
+    else:
+      weather = {}
+      api_address = "https://api.openweathermap.org/data/2.5/weather?q={}" \
+                    "&appid={}" \
+                    "&units=metric".format(args[0], config['owappid'])
+      json_data = requests.get(api_address).json()
+      weather['current'] = json_data['main']['temp']
+      weather['pressure'] = json_data['main']['pressure']
+      weather['humidity'] = json_data['main']['humidity']
+      weather['temp_max'] = json_data['main']['temp_max']
+      weather['temp_min'] = json_data['main']['temp_min']
+      bot_return['message'] = "Current temperature: *{}°C*\n" \
+                              "Pressure: *{}Pa*\n" \
+                              "Humidity: *{}%*\n" \
+                              "Maximum temperature today: *{}°C*\n" \
+                              "Minimum temperature today: *{}°C*".format(weather['current'],
+                                                                         weather['pressure'],
+                                                                         weather['humidity'],
+                                                                         weather['temp_max'],
+                                                                         weather['temp_min']),
 
-    bot_return['block'] = False
-    return bot_return
+      bot_return['block'] = False
+      bot_return['error'] = False
+      bot_return['reactions'] = []
+      return bot_return
+
+
